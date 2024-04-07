9ab378ca8ebea7ae27ac394064d318e6de0ee22a
wei <wendyhsu09@gmail.com>
2024-04-07 21:52:05 +0800

    add:1.required parameters 2.optional parameters 3. regular expression constraints and global constraints

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+        Route::pattern('id','[0-9]+');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return 'contact';
 })->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {
+    return 'Blog post ' . $id;
+})
+// ->where([
+//     'id'=>'[0-9]+'
+// ])
+->name('posts.show');
+
+Route::get('/recent-posts/{datys_ago?}', function ( $daysAgo = 20) {
+    return 'Posts from' . $daysAgo . ' days ago';
+})->name('posts.recent.index');
